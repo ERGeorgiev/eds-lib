4733dbd7688215373ef4a2ec472132f3d73d5571
Eduard <e.r.georgiev@gmail.com>
2024-05-15 23:38:42 +0100

    Added embedded resources

--- EdsLibrary/Extensions/ImageExt.cs
+++ EdsLibrary/Extensions/ImageExt.cs
@@ -8,6 +8,11 @@ public static partial class ImageExt
     public static Lazy<Image> Blank = new(() => BitmapExt.Blank.Value.ToImage());
 
     /// <summary>
+    /// Low quality conversion. Finding a high quality conversion proved difficult.
+    /// </summary>
+    public static Icon ToIcon(this Image img) => new Bitmap(img).ToIcon();
+
+    /// <summary>
     /// Transforms an image into a byte array.
     /// </summary>
     public static byte[] ToByteArray(this Image image)
@@ -20,8 +25,10 @@ public static partial class ImageExt
     /// <summary>
     /// Load an image from a file. Returns a blank fallback image on failure.
     /// </summary>
-    public static Image FromFileSafe(this string filepath)
+    public static Image FromFileSafe(this string? filepath)
     {
+        if (string.IsNullOrEmpty(filepath)) return Blank.Value;
+
         try
         {
             return Image.FromFile(filepath);
@@ -29,6 +36,24 @@ public static partial class ImageExt
         catch (Exception e)
         {
             Logger.LogError($"Failed to load image '{filepath}'", e);
+            return Blank.Value;
+        }
+    }
+
+    /// <summary>
+    /// Load an image from a file. Returns a blank fallback image on failure.
+    /// </summary>
+    public static Image FromStreamSafe(this Stream? stream)
+    {
+        if (stream == null) return Blank.Value;
+
+        try
+        {
+            return Image.FromStream(stream);
+        }
+        catch (Exception e)
+        {
+            Logger.LogError($"Failed to load image from stream.", e);
             return Blank.Value;
         }
     }
---
+++ EdsLibrary/Utility/EmbeddedResources.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace EdsLibrary.Utility;
+
+public static class EmbeddedResources
+{
+    public static Stream? Get(string filename, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
+    {
+        var callingAssembly = Assembly.GetCallingAssembly();
+        var manifestResources = GetManifestResourceNamesThatContainFile(callingAssembly, filename, comparisonType);
+        if (manifestResources.Count() > 1) Logger.LogWarning($"More than a single assembly manifest resource contains file '{filename}', using the first instance.");
+
+        var manifestResource = manifestResources.FirstOrDefault();
+        if (string.IsNullOrEmpty(manifestResource))
+        {
+            Logger.LogError($"Failed to load embedded resource '{filename}', no such found.");
+            return null;
+        }
+
+        return Assembly.GetCallingAssembly().GetManifestResourceStream(manifestResource);
+    }
+
+    private static IEnumerable<string> GetManifestResourceNamesThatContainFile(Assembly assembly, string filename, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
+    {
+        var names = assembly.GetManifestResourceNames().Where(v => v.EndsWith(filename, comparisonType));
+        return names;
+    }
+}
