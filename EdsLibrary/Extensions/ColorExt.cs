942aeb01d4cf6af2ede8b74bf6f34561b0bf2d7a
Eduard <e.r.georgiev@gmail.com>
2024-07-03 22:42:02 +0100

    Added Color Merge

---
+++ EdsLibrary/Extensions/ColorExt.cs
@@ -0,0 +1,17 @@
+﻿namespace EdsLibrary.Extensions;
+
+/// <summary>
+/// Extensions for the <see cref="Color"/> type.
+/// </summary>
+public static partial class ColorExt
+{
+    public static Color Merge(this Color a, Color b)
+    {
+        var alpha = 255 - ((255 - a.A) * (255 - b.A) / 255);
+        var red = (a.R * (255 - b.A) + b.R * b.A) / 255;
+        var green = (a.G * (255 - b.A) + b.G * b.A) / 255;
+        var blue = (a.B * (255 - b.A) + b.B * b.A) / 255;
+
+        return Color.FromArgb(alpha, red, green, blue);
+    }
+}
