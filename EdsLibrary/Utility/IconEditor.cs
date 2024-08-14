7679715938ca72a3baa9a47ebd8ee2310e73aa32
Eduard <e.r.georgiev@gmail.com>
2024-08-14 19:12:46 +0100

    Added a warning in the icon editor

--- EdsLibrary/Utility/IconEditor.cs
+++ EdsLibrary/Utility/IconEditor.cs
@@ -7,5 +7,10 @@ public static class IconEditor
 {
     public static Icon Flip(Icon icon, Plane? plane = null) => icon.ToBitmap().Flip(plane).ToIcon();
     public static Icon Rotate(Icon icon, double angle) => icon.ToBitmap().Rotate(angle).ToIcon();
+
+    // ToDo: This causes GDI crash. Maybe because of multi-threading in IconEditor.
+    // Tho now I was not doing anything and it still crashed. Is it a memory issue, as it seems to happen overtime? Idk..
+    // https://stackoverflow.com/questions/1053052/a-generic-error-occurred-in-gdi-jpeg-image-to-memorystream
+    // Maybe use Icon.Clone
     public static Icon Overlay(Icon layer1, Icon layer2) => layer1.ToBitmap().Overlay(layer2.ToBitmap()).ToIcon();
 }
