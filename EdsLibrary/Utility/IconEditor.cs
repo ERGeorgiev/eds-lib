80034f0ed7211f42f2d78ab444b4dd4e6f53cd5c
Eduard <e.r.georgiev@gmail.com>
2024-08-14 15:29:05 +0100

    Expanded IconEditor

--- EdsLibrary/Utility/IconEditor.cs
+++ EdsLibrary/Utility/IconEditor.cs
@@ -5,9 +5,7 @@ namespace EdsLibrary.Utility;
 
 public static class IconEditor
 {
-    public static Icon Flip(Icon icon, Plane? plane = null)
-    {
-        using var newIcon = icon.ToBitmap().Flip(plane);
-        return newIcon.ToIcon();
-    }
+    public static Icon Flip(Icon icon, Plane? plane = null) => icon.ToBitmap().Flip(plane).ToIcon();
+    public static Icon Rotate(Icon icon, double angle) => icon.ToBitmap().Rotate(angle).ToIcon();
+    public static Icon Overlay(Icon layer1, Icon layer2) => layer1.ToBitmap().Overlay(layer2.ToBitmap()).ToIcon();
 }
